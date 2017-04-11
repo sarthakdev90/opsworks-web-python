268807cd4096fb516e3479e34d51ad1288f2dcd9
Sarthak Dev <sarthakdev90@gmail.com>
2017-04-11 14:38:43 +0530

    Adding pip script inside loop

--- opsworks_deploy_python/recipes/default.rb
+++ opsworks_deploy_python/recipes/default.rb
@@ -7,20 +7,6 @@ include_recipe 'poise-python'
 include_recipe 'apt::default'
 include_recipe 'gunicorn'
 
-Chef::Log.info "Installing Pip"
-easy_install_package 'pip' do
-  module_name 'pip'
-  action :install
-end
-
-python_runtime '2'
-python_virtualenv '/.virtualenvs/test/'
-
-Chef::Log.info "Installing Django"
-python_package 'Django' do
-  version '1.8'
-end
-
 node[:deploy].each do |application, deploy|
   Chef::Log.info "Application is #{application}"
   Chef::Log.info "Custom type is #{deploy["custom_type"]}"
@@ -37,6 +23,21 @@ node[:deploy].each do |application, deploy|
 #     app_name application
 #   end
 # node[:deploy].each do |application, deploy|
+
+  Chef::Log.info "Installing Pip"
+  easy_install_package 'pip' do
+    module_name 'pip'
+    action :install
+  end
+
+  python_runtime '2'
+  python_virtualenv '/.virtualenvs/test/'
+
+  Chef::Log.info "Installing Django"
+  python_package 'Django' do
+    version '1.8'
+  end
+
 
   opsworks_deploy_dir do
     user deploy[:user]
