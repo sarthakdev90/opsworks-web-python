0aad0be3a595e0db25d63d6f6b9c181f7a86fc1e
Sarthak Dev <sarthakdev90@gmail.com>
2017-04-11 15:21:41 +0530

    Commenting everything out

--- opsworks_deploy_python/metadata.rb
+++ opsworks_deploy_python/metadata.rb
@@ -16,21 +16,21 @@ depends "scm_helper"
 depends "apt"
 depends "opsworks_initial_setup"
 
-gem "poise"
-gem "poise-python"
-gem "berkshelf"
+# gem "poise"
+# gem "poise-python"
+# gem "berkshelf"
 
-# depends 'application', '~> 5.0'
-# depends 'application_git', '~> 1.0'
-# depends 'application_javascript', '~> 1.0'
-# depends 'application_python', '~> 4.0'
-# depends 'application_ruby', '~> 4.0'
-# depends 'build-essential'
-depends 'poise', '~> 2.0'
-depends 'poise-python'
-depends 'poise-archive', '~> 1.3'
-depends 'apt', '~> 6.0.1'
-depends 'gunicorn', '~> 2.0.0'
+# # depends 'application', '~> 5.0'
+# # depends 'application_git', '~> 1.0'
+# # depends 'application_javascript', '~> 1.0'
+# # depends 'application_python', '~> 4.0'
+# # depends 'application_ruby', '~> 4.0'
+# # depends 'build-essential'
+# depends 'poise', '~> 2.0'
+# depends 'poise-python'
+# depends 'poise-archive', '~> 1.3'
+# depends 'apt', '~> 6.0.1'
+# depends 'gunicorn', '~> 2.0.0'
 
 recipe "opsworks_deploy_python", "Install and setup a python application in a virtualenv"
 recipe "opsworks_deploy_python::buildout", "Install and setup a buildout based python application"
